107c99f047b48e34f012356b15ae1d35e157d8ef
Jatin Paul <jatinpaul443@gmail.com>
2025-07-01 15:11:59 +0530

    Adding share meals page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,10 +1,39 @@
+import { getMealDetails } from "@/lib/meals";
 import React from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
+  const meal = getMealDetails(params.slug);
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <div>
-      <h1>Meal Detais page</h1>
-      <p>{params.slug}</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailTo: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function MealsError() {
+  return (
+    <main className="error">
+      <h1>An Error Occured!</h1>
+      <p>Failed to fetch meals</p>
+    </main>
+  );
+}
