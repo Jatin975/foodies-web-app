1de6db04e6e12b8d061e72caea0b785355334716
Jatin Paul <jatinpaul443@gmail.com>
2025-07-02 15:09:06 +0530

    Managing errors while form submit for share meals

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,13 @@
+'use client';
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealFormSubmit from "@/components/meals/meal-form-submit";
+import { useActionState } from "react";
 
 export default function ShareMealPage() {
+  const [currState, formAction] = useActionState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +17,8 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
+            {currState.message && <p>{currState.message}</p>}
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
