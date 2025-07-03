c75aa867a4bb90c75c34efc9e7ef587a2be11ed5
Jatin Paul <jatinpaul443@gmail.com>
2025-07-03 13:48:47 +0530

    Adding metadata for other pages

--- app/meals/page.js
+++ app/meals/page.js
@@ -10,6 +10,11 @@ async function Meals() {
   return <MealsGrid meals={meals} />;
 }
 
+export const metadata = {
+  title: "All meals",
+  description: "Browse delicious meals shared by our vibrant community.",
+};
+
 export default async function MealsPage() {
   return (
     <>
